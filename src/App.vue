4948c1c7198526154e43daac5fa57ae9eac5fda6
WawanC <kurniawancristianto@gmail.com>
2021-08-23 21:20:38 +0700

    Improve CSS Design

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,38 @@
   <h1>VueJS Turn-Based Game</h1>
   <div class="healthGUI">
     <h2>Enemy</h2>
-    <div class="enemyHealthBar" :style="enemyHealthStyles"></div>
+    <div class="healthContainer">
+      <div class="enemyHealthBar" :style="enemyHealthStyles"></div>
+    </div>
   </div>
   <div class="healthGUI">
     <h2>Player</h2>
-    <div class="playerHealthBar" :style="playerHealthStyles"></div>
+    <div class="healthContainer">
+      <div class="playerHealthBar" :style="playerHealthStyles"></div>
+    </div>
   </div>
   <div class="endGameGUI" v-if="gameWinner">
     <h3>{{ gameWinner }} win the game !</h3>
     <button @click="restartGame">Restart Game</button>
   </div>
   <div class="gameControlsGUI" v-if="gameWinner === null">
-    <button @click="attackEnemy">Attack</button>
-    <button @click="specialAttackEnemy" :disabled="!specialAttackAvailability">
-      Special Attack
+    <button @click="attackEnemy" id="attackButton">
+      <i class="fa fa-hand-rock-o" aria-hidden="true"></i> Attack
     </button>
-    <button @click="healPlayer" :disabled="!healAvailability">Heal</button>
+    <button
+      @click="specialAttackEnemy"
+      :disabled="!specialAttackAvailability"
+      id="specialButton"
+    >
+      <i class="fa fa-hand-lizard-o" aria-hidden="true"></i> Special Attack
+    </button>
+    <button @click="healPlayer" :disabled="!healAvailability" id="healButton">
+      <i class="fa fa-heart" aria-hidden="true"></i> Heal
+    </button>
   </div>
   <div class="battleLogGUI">
-    <h3>Battle Log</h3>
-    <ul v-if="battleLog.length > 0">
+    <h3>----- Battle Log -----</h3>
+    <ul class="battleLogBox">
       <li v-for="log in battleLog" :key="log">{{ log }}</li>
     </ul>
   </div>
@@ -123,14 +135,43 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap");
+body,
+button {
+  font-family: "Roboto Mono";
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 .healthGUI {
-  width: 25%;
-  position: relative;
+  width: 40%;
+  display: flex;
+  flex-direction: row;
+}
+.healthGUI h2 {
+  flex: 1;
+}
+.healthContainer {
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  overflow: hidden;
+  background-color: gainsboro;
+  border: 1px solid black;
+  padding: 0;
+  flex: 3;
 }
 .enemyHealthBar,
 .playerHealthBar {
-  height: 50px;
-  border-radius: 10px;
+  height: 100%;
 }
 .enemyHealthBar {
   background-color: red;
@@ -138,23 +179,43 @@ export default {
 .playerHealthBar {
   background-color: green;
 }
-.enemyHealthBar::before,
-.playerHealthBar::before {
-  content: "";
-  position: absolute;
-  z-index: -1;
-  width: 100%;
-  border-radius: 10px;
-  height: 50px;
-  background-color: grey;
-}
 .gameControlsGUI {
+  display: flex;
+  width: 40%;
+  flex-direction: row;
+  justify-content: center;
+  gap: 5%;
   padding: 20px;
+}
+#attackButton {
+  background-color: salmon;
+}
+#specialButton {
+  background-color: yellow;
+}
+#healButton {
+  background-color: greenyellow;
 }
 .gameControlsGUI button,
 .endGameGUI button {
   font-size: 16pt;
   padding: 15px;
   border-radius: 10px;
+}
+.battleLogGUI {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  height: 40%;
+  width: 40%;
+}
+.battleLogBox {
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  border: 1px solid black;
+  background-color: gainsboro;
+  overflow: auto;
 }
 </style>
